6f90d6f80c2d2bdc08652ccd1103d4e59b3253c3
Shinugamieyes420 <remon131090@gmail.com>
2026-08-23 04:26:58 +0200

    fix: immersive fullscreen on Android landscape

--- scripts/patch-android.mjs
+++ scripts/patch-android.mjs
@@ -1,5 +1,13 @@
 import fs from 'node:fs';
+
 const manifest='android/app/src/main/AndroidManifest.xml';
 let s=fs.readFileSync(manifest,'utf8');
-s=s.replace('<activity','<activity android:screenOrientation="landscape"');
+if(!s.includes('android:screenOrientation="landscape"')) s=s.replace('<activity','<activity android:screenOrientation="landscape"');
 fs.writeFileSync(manifest,s);
+
+const mainActivity='android/app/src/main/java/com/remon/pokemonlaneclash/MainActivity.java';
+if(fs.existsSync(mainActivity)){
+  let m=fs.readFileSync(mainActivity,'utf8');
+  m=`package com.remon.pokemonlaneclash;\n\nimport android.os.Bundle;\nimport android.view.View;\nimport com.getcapacitor.BridgeActivity;\n\npublic class MainActivity extends BridgeActivity {\n  @Override\n  public void onCreate(Bundle savedInstanceState) {\n    super.onCreate(savedInstanceState);\n    hideSystemUI();\n  }\n\n  @Override\n  public void onWindowFocusChanged(boolean hasFocus) {\n    super.onWindowFocusChanged(hasFocus);\n    if (hasFocus) hideSystemUI();\n  }\n\n  private void hideSystemUI() {\n    getWindow().getDecorView().setSystemUiVisibility(\n      View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |\n      View.SYSTEM_UI_FLAG_FULLSCREEN |\n      View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |\n      View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |\n      View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |\n      View.SYSTEM_UI_FLAG_LAYOUT_STABLE\n    );\n  }\n}\n`;
+  fs.writeFileSync(mainActivity,m);
+}
